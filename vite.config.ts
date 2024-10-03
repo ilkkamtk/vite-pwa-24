17ffbd64804752b7f387201ac70e6e33483c256b
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-10-03 11:23:52 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,39 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
+        type: 'module',
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf}'],
+      },
+      includeAssets: ['app-icon.svg', 'main.css', 'Pacifico-Regular.ttf'],
+      manifest: {
+        name: 'vite pwa example',
+        short_name: 'vite-pwa',
+        description: 'example for vite pwa plugin',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     }),
   ],
